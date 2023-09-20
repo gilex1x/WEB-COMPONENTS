687855b1ae49102c25d5ad4dd3ff0ceed3832731
Gilberto <gilbertosa099@gmail.com>
2023-09-19 21:19:50 -0500

    [ADD]Observer to changed atributes

--- resources/template.js
+++ resources/template.js
@@ -1,19 +1,24 @@
-class myElement extends HTMLElement{
-    constructor(){
-        //Aca definimos lo que va a estar en  nuestro element
-        super();
-        this.attachShadow({mode: 'open'});//Esto me permite interactuar y ver el contenido del componente
-        //Para obtener cualquier elemento dentro del shadow dom usamos shadowroot en lugar del document
+class myElement extends HTMLElement {
+  constructor() {
+    //Aca definimos lo que va a estar en  nuestro element
+    super();
+    this.attachShadow({ mode: "open" }); //Esto me permite interactuar y ver el contenido del componente
+    //Para obtener cualquier elemento dentro del shadow dom usamos shadowroot en lugar del document
+  }
+  //Añadimos un observador de los atributos
+  static get observedAttributes() {
+    return ["title", "paragraph", "image"];
+  }
 
-        // vamos a optener los atributos 
-        this.title = this.getAttribute('title');
-        this.paragraph = this.getAttribute('paragraph');
-        this.image = this.getAttribute('image');
+  attributeChangedCallback(current, oldValue, newValue) {
+    if(oldValue !== newValue){
+        this[current] = newValue;
     }
+  }
 
-    getTemplate(){
-        const template = document.createElement('template');
-        template.innerHTML = `
+  getTemplate() {
+    const template = document.createElement("template");
+    template.innerHTML = `
         <section>
             <h2>${this.title}</h2>
             <section>
@@ -23,28 +28,28 @@ class myElement extends HTMLElement{
         </section>
         ${this.setSyles()}`;
 
-        return template;
-    }
+    return template;
+  }
 
-    render(){
-        //Agregamos el template al shadow root (sahdow DOM)
-        this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));//creamos una copia del template
-    }
+  render() {
+    //Agregamos el template al shadow root (sahdow DOM)
+    this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true)); //creamos una copia del template
+  }
 
-    setSyles(){
-        let styles= `
+  setSyles() {
+    let styles = `
         <style>
             h2{ font-weight: bold}
             p{ color: red}
         </style>
         `;
-        return styles;
-    }
+    return styles;
+  }
 
-    connectedCallback(){
-        //Aca renderizamos todo
-        this.render();
-    }
-};
+  connectedCallback() {
+    //Aca renderizamos todo
+    this.render();
+  }
+}
 
-customElements.define('my-element',myElement);
+customElements.define("my-element", myElement);
